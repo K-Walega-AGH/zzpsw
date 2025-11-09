ee20c78f8999aa834d23deaa93efd45057fd7f7f
unknown <kwalega@student.agh.edu.pl>
2025-11-09 22:30:45 +0100

    3.1_Task2

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -1,31 +1,36 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "led.h"
-void Delay(unsigned int uiMiliSec) {
-	unsigned int uiLoopCtr, uiDelayLoopCount;
-	uiDelayLoopCount = uiMiliSec*12000;
-	for(uiLoopCtr=0;uiLoopCtr<uiDelayLoopCount;uiLoopCtr++){
-	}
-}
-void Led0Blink( void *pvParameters ){
-while(1){
-	Led_Toggle(0);
-	Delay(500);
 
-	}
-}
+unsigned char ucBlinkingFreq = 1;
 
-void Led1Blink( void *pvParameters ){
-while(1){
-	Led_Toggle(1);
-	Delay(500);
+
+void LedBlink( void *pvParameters ){
+	unsigned char *pucFreq = (unsigned char*)pvParameters;
+	while(1){
+		Led_Toggle(0);
+		vTaskDelay((1000/(*pucFreq))/2);
 	}
 }
 
-int main(void){
+void LedCtrl (void *pvChangingFreq){
+	unsigned char ucFreq = *((unsigned char*)pvChangingFreq);
+	while(1){
+		vTaskDelay(1000);
+		ucFreq = ucFreq +1;
+		ucBlinkingFreq = ucFreq;
+	}
+
+	
+}
+
+
+int main( void )
+{
 	Led_Init();
-	xTaskCreate(Led0Blink, NULL , 99 , NULL, 2 , NULL );
-	xTaskCreate(Led1Blink, NULL , 100 , NULL, 2 , NULL );
+	xTaskCreate(LedBlink, NULL , 100 , &ucBlinkingFreq, 2 , NULL );
+	xTaskCreate(LedCtrl, NULL , 100 , &ucBlinkingFreq, 2 , NULL );
 	vTaskStartScheduler();
 	while(1);
 }
+
