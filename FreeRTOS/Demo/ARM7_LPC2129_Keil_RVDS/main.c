115c90e4e91ef471c00f155402c0a07d665951eb
unknown <kwalega@student.agh.edu.pl>
2025-12-27 19:34:14 +0100

    Queue_Zad_1

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -6,37 +6,45 @@
 #include "semphr.h"
 #include "stdio.h"
 
-xSemaphoreHandle xSemaphore;
+#define QUEUE_LEN   5
+#define STR_LEN     32
 
-void Rtos_Transmiter_SendString (char cString[]){
-	xSemaphoreTake(xSemaphore, portMAX_DELAY);
-	Transmiter_SendString(cString);
-	while (eTransmiter_GetStatus()!=FREE){};
-	xSemaphoreGive(xSemaphore);
+QueueHandle_t xQueue;
+
+void UartTxTask(void *pvParameters)
+{
+    char cBuffer[STR_LEN];
+
+    while (1)
+    {
+        if (xQueueReceive(xQueue, cBuffer, portMAX_DELAY) == pdTRUE)
+        {
+            Transmiter_SendString(cBuffer);
+            while (eTransmiter_GetStatus() != FREE) {}
+        }
+    }
 }
 
 void LettersTx (void *pvParameters){
-	char cBuffer[22];
+	char cBuffer[STR_LEN];
 	unsigned int uiPassedTime=0;
 	while(1){
 		uiPassedTime = (unsigned int)xTaskGetTickCount();
-		Rtos_Transmiter_SendString(cBuffer);
+		xQueueSend(xQueue, cBuffer, 0);
 		uiPassedTime = (unsigned int)xTaskGetTickCount() - uiPassedTime;
 		CopyString("-ABCDEEFGH-:",cBuffer);
 		AppendUIntToString(uiPassedTime,cBuffer);
 		AppendString("\n",cBuffer);
-		
-		
 		vTaskDelay(300);
 	}
 }
 
 void KeyboardTx (void *pvParameters) {
 	enum KeyboardState eLastState = RELASED;
-	
+	char cBuffer[STR_LEN] = "-Keyboard-\n";
 	while(1){
 		if (eKeyboardRead() != RELASED && eLastState == RELASED){
-			Rtos_Transmiter_SendString("-Keyboard-\n");
+			xQueueSend(xQueue, cBuffer, 0);
 		}
 		eLastState = eKeyboardRead();
 		vTaskDelay(300);
@@ -45,8 +53,8 @@ void KeyboardTx (void *pvParameters) {
 
 int main( void ){
 	UART_InitWithInt(300);
-	vSemaphoreCreateBinary(xSemaphore);
-	
+	xQueue = xQueueCreate(QUEUE_LEN, STR_LEN);
+	xTaskCreate(UartTxTask,  NULL, 128, NULL, 2, NULL);
 	xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL );
 	xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL );
 	vTaskStartScheduler();
