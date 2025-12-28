9b8952359ebc9b643c0581bb9381c0f09222e167
unknown <kwalega@student.agh.edu.pl>
2025-12-28 23:41:20 +0100

    Kolejki_Zad_1_TymRazemDobrze

--- FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ FreeRTOS/Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -6,23 +6,25 @@
 #include "semphr.h"
 #include "stdio.h"
 
+xSemaphoreHandle xSemaphore;
+
 #define QUEUE_LEN   5
 #define STR_LEN     32
 
 QueueHandle_t xQueue;
 
-void UartTxTask(void *pvParameters)
-{
-    char cBuffer[STR_LEN];
 
-    while (1)
-    {
-        if (xQueueReceive(xQueue, cBuffer, portMAX_DELAY) == pdTRUE)
-        {
-            Transmiter_SendString(cBuffer);
-            while (eTransmiter_GetStatus() != FREE) {}
-        }
-    }
+
+void Rtos_Transmiter_SendString (void *pvParameters){
+	while(1){
+		char cBuffer[STR_LEN];
+		if(xQueueReceive(xQueue, cBuffer, portMAX_DELAY) == pdPASS){
+				xSemaphoreTake(xSemaphore, portMAX_DELAY);
+				Transmiter_SendString(cBuffer);
+				while (eTransmiter_GetStatus()!=FREE){};
+				xSemaphoreGive(xSemaphore);
+		}
+	}
 }
 
 void LettersTx (void *pvParameters){
@@ -34,29 +36,30 @@ void LettersTx (void *pvParameters){
 		uiPassedTime = (unsigned int)xTaskGetTickCount() - uiPassedTime;
 		CopyString("-ABCDEEFGH-:",cBuffer);
 		AppendUIntToString(uiPassedTime,cBuffer);
-		AppendString("\n",cBuffer);
-		vTaskDelay(300);
+		AppendString("\n",cBuffer);		
+		vTaskDelay(1000);
 	}
 }
 
 void KeyboardTx (void *pvParameters) {
 	enum KeyboardState eLastState = RELASED;
-	char cBuffer[STR_LEN] = "-Keyboard-\n";
 	while(1){
 		if (eKeyboardRead() != RELASED && eLastState == RELASED){
-			xQueueSend(xQueue, cBuffer, 0);
+			xQueueSend(xQueue, "-Keyboard-\n", portMAX_DELAY);
 		}
 		eLastState = eKeyboardRead();
-		vTaskDelay(300);
+		vTaskDelay(50);
 	}
 }
 
 int main( void ){
 	UART_InitWithInt(300);
+	vSemaphoreCreateBinary(xSemaphore);
 	xQueue = xQueueCreate(QUEUE_LEN, STR_LEN);
-	xTaskCreate(UartTxTask,  NULL, 128, NULL, 2, NULL);
 	xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL );
 	xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL );
+	xTaskCreate(Rtos_Transmiter_SendString, NULL, 128, NULL, 1, NULL );
+
 	vTaskStartScheduler();
 	while(1);
 }
